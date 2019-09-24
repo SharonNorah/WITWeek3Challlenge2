4f896af57cfc82a5db638c353ccb43a6afa9ce0d
akolsharonnorah@gmail.com <akolsharonnorah@gmail.com>
2019-09-24 21:16:35 +0300

    modified account.js

--- account.js
+++ account.js
@@ -1,57 +1,54 @@
 class BankAccount {
-    constructor(name, balance, accountNumber, accountStatus) {
-        this.name = name;
-        this.balance = 0;
-        this.accountStatus = accountStatus;
-        this.accountNumber = accountNumber;
-    }
+  constructor(name, accountNumber, initialDeposit, accountStatus) {
+    this.name = name;
+    this.initialDeposit = initialDeposit;
+    this.accountStatus = accountStatus;
+    this.accountNumber = accountNumber;
+  }
 
-    getBalance(amount) {
-        if (this.accountStatus === 'active' && (this.balance = 0)) {
-            this.balance += amount;
-            return `${this.name}, your balance is ${this.balance}`
-        }
-        return `this account is inactive`;
+  getBalance() {
+    if (
+      this.accountStatus === "active" &&
+      (this.balance = this.initialDeposit)
+    ) {
+      // this.balance += initialDeposit;
+      return `${this.name}, your balance is ${this.balance}`;
     }
+    return `this account is inactive`;
+  }
 
-    open(name, accountNumber) {
-        if (typeof this.name === 'string' && (Number.isInteger(accountNumber))) {
-            return `${this.name}, your account has been opened your account number is ${this.accountNumber}`;
-        }
-        return "name must be characters and accountNumber must be an integer";
+  open(name, accountNumber, initialDeposit) {
+    if (typeof this.name === "string" && Number.isInteger(accountNumber)) {
+      return `${this.name}, your account has been opened with account number: ${this.accountNumber}, initialDeposit: ${this.initialDeposit}`;
     }
-    deposit(amount) {
-        if (this.balance >= 0 && this.accountStatus === 'active') {
-            this.balance += amount;
-            return (`${this.name}, you have deposited ${amount} and your new balance is ${this.balance}`);
-        }
-        return "this account is inactive";
+    return "name must be characters and accountNumber must be an integer";
+  }
+  deposit(amount) {
+    if (this.balance >= 0 && this.accountStatus === "active") {
+      this.balance += amount;
+      return `${this.name}, you have deposited ${amount} and your new balance is ${this.balance}`;
     }
-    withdraw(amount) {
-        this.balance -= amount;
-        if (this.balance - amount >= 0 && this.accountStatus === 'active') {
-            return (`${this.name}, you have withdrawn ${amount} and your new balance is ${this.balance}`);
-        }
-        return "you either have insufficient funds or the account is closed";
+    return "this account is inactive";
+  }
+  withdraw(amount) {
+    this.balance -= amount;
+    if (this.balance - amount >= 0 && this.accountStatus === "active") {
+      return `${this.name}, you have withdrawn ${amount} and your new balance is ${this.balance}`;
     }
-    close(accountStatus) {
-        if (this.accountStatus === 'inactive') {
-            return "this account is closed, you cant carry out transactions";
-        }
-        return `${this.name}, the account is active, carry out your transactions`;
-    }
+    return "you have insufficient funds";
+  }
+  //set the accountsatus to inactive
+  close() {
+    this.accountStatus === "inactive";
+    return "this account is closed, you cant carry out transactions";
+  }
 }
 module.exports = BankAccount;
-let client = new BankAccount('sharon', 0, 22850, 'active');
-console.log(client.open('sharon', 22850));
-console.log(client.getBalance(0));
-console.log(client.deposit(5000));
-console.log(client.withdraw(2000));
+let client = new BankAccount("sharon", 22850, 5000, "active");
+//console.log(client.open("sharon", 22850, 5000));
+//console.log(client.getBalance(5000));
+//console.log(client.deposit(5000));
+//console.log(client.withdraw(2000));
 console.log(client.close());
 
-client = new BankAccount('norah', 0, 22440, 'inactive');
-console.log(client.open('norah', 22440));
-console.log(client.getBalance(0));
-console.log(client.deposit(5000));
-console.log(client.withdraw(2000));
-console.log(client.close());
+//client = new BankAccount('norah', 0, 22440, 'inactive');
--- account.test.js
+++ account.test.js
@@ -1,38 +1,53 @@
-
-const BankAccount = require('./account.js');
-const client = new BankAccount('sharon', 0, 22850, 'active');
-test('open account withname and account number', () => {
-    expect(client.open('sharon', 22850)).toBe; "sharon, your account has been opened your account number is 22850"
+const BankAccount = require("./account.js");
+const client = new BankAccount("sharon", 0, 22850, "active");
+test("open account withname and account number", () => {
+  expect(client.open("sharon", 22850)).toBe(
+    "sharon, your account has been opened your account number is 22850"
+  );
 });
-test('name must be string and accountnumber must be an integer', () => {
-    expect(client.open('sharon', 'aa23')).toBe; "name must be characters and accountNumber must be an integer"
+test("name must be string and accountnumber must be an integer", () => {
+  expect(client.open("sharon", "aa23")).toBe(
+    "name must be characters and accountNumber must be an integer"
+  );
 });
-test('newly open account has zero balance', () => {
-    expect(client.getBalance()).toBe; '${this.name}, your balance is ${this.balance}'
-});
-
-test('can deposit money in active account', () => {
-    expect(client.deposit(5000)).toBe; 'sharon, you have deposited 5000 and your new balance is ${this.balance'
-});
-
-test('can withdraw money from active account', () => {
-    expect(client.withdraw(2000)).toBe; 'sharon, you have withdrawn 2000 and your new balance is 3000'
+test("newly open account has zero balance", () => {
+  expect(client.getBalance()).toBe(
+    "${this.name}, your balance is ${this.balance}"
+  );
 });
 
-test('check if the account is still active', () => {
-    expect(client.close()).toBe; "sharon, the account is active, carry out your transactions"
-});
-test('check if account is closed', () => {
-    expect(client.close()).toBe; 'this account is closed, you cant carry out transactions'
-});
-
-
-client1 = new BankAccount('norah', 0, 22440, 'inactive');
-test('can deposit money in closed account', () => {
-    expect(client.deposit(5000)).toBe; "this account is inactive"
-});
-test('can withdraw money from a closed account', () => {
-    expect(client.withdraw(2000)).toBe; "you either have insufficient funds or the account is closed"
+test("can deposit money in active account", () => {
+  expect(client.deposit(5000)).toBe(
+    "sharon, you have deposited 5000 and your new balance is ${this.balance"
+  );
 });
 
+test("can withdraw money from active account", () => {
+  expect(client.withdraw(2000)).toBe(
+    "sharon, you have withdrawn 2000 and your new balance is 3000"
+  );
+});
 
+test("check if the account is still active", () => {
+  expect(client.close()).toBe(
+    "sharon, the account is active, carry out your transactions"
+  );
+});
+test("check if account is closed", () => {
+  expect(client.close()).toBe(
+    "this account is closed, you cant carry out transactions"
+  );
+});
+
+client1 = new BankAccount("norah", 0, 22440, "inactive");
+test("can deposit money in closed account", () => {
+  expect(client.deposit(5000)).toBe;
+  ("this account is inactive");
+});
+test("can withdraw money from a closed account", () => {
+  expect(client.withdraw(2000)).toBe;
+  ("you have insufficient funds");
+});
+
+//include tests with an opening balance
+//
